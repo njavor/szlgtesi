d3970426f8e0290066dbb878b748e4e158303fed
Niki Jávor <nikolettjavor@gmail.com>
2022-05-16 19:25:32 +0200

    view forgolódás, kezdetleges igh-i felület

--- jelenletiv/views.py
+++ jelenletiv/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+import datetime
+import re
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 from .models import Alkalom, Foglalkozas
 
@@ -9,17 +12,32 @@ def indexview(request):
     context = {}
     try:
         filter = Foglalkozas.objects.get(edzo = request.user)
-        context['alkalmak'] = Alkalom.objects.filter(foglalkozas = filter)
+        return HttpResponseRedirect(filter.url + '/')
     except:
         context['foglalkozasok'] = Foglalkozas.objects.all()
+        return render(request, "ighely.html", context)
 
-    return render(request, "index.html", context)
+@login_required
+def foglalkozasview(request, fog):
+    context = {'alkalmak': Alkalom.objects.filter(foglalkozas__url = fog).order_by('-datum'),}
+    if not request.user.is_staff:
+        try:
+            ma = Alkalom.objects.filter(foglalkozas__url = fog).get(datum=datetime.datetime.now())
+            context['alkalmak'] = Alkalom.objects.filter(foglalkozas__url = fog).exclude(datum = ma.datum).order_by('-datum')
+            context['ma'] = ma
+        except: pass
+    else:
+        context['staff'] = True
+    return render(request, "foglalkozas.html", context)
+    
 
 @login_required
 def alkalomview(request, fog, alk):
-    print(fog + " - ez a fog")
     context = {
         'azalkalom': Alkalom.objects.filter(foglalkozas__url = fog).get(pk = alk),
         'resztvevok': Foglalkozas.objects.get(url = fog).diakok.all(),
     }
+    if request.user.is_staff:
+        context['staff'] = True
     return render(request, "alkalom.html", context)
+
--- mt_jelenletiv/urls.py
+++ mt_jelenletiv/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from jelenletiv.views import indexview, alkalomview
+from jelenletiv.views import foglalkozasview, indexview, alkalomview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('django.contrib.auth.urls')),
     path('', indexview, name="index"),
+    path('<str:fog>/', foglalkozasview, name="foglalkozas"),
     path('<str:fog>/<int:alk>/', alkalomview, name="alkalom"),
 ]
